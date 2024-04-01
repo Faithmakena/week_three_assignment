22dd9ed4a3578fdaa0b2f04910009d0858ad7876
Faithmakena <faiy2makena@gmail.com>
2024-04-01 22:24:41 +0300

    first commit

---
+++ week_one_assignment/user_input.py
@@ -0,0 +1,11 @@
+# Asking user for their name
+name = input("What is your name? ")
+
+# Asking user for their age
+age = input("How old are you? ")
+
+# Asking user for their location
+location = input("Where are you located? ")
+
+# Printing personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
